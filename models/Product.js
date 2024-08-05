e0b1c315a09a835967a0713b7541b90fb7690937
Alfredo Esparza Snyder <alfredoesparza250@gmail.com>
2024-08-04 21:38:41 -0600

    Moved project to 'ONLINE STORE BACKEND' directory

--- models/Product.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ProductSchema = new mongoose.Schema({
-    _id: String,  // Define _id como String
-    name: String,
-    price: Number
-});
-
-module.exports = mongoose.model('Product', ProductSchema);
